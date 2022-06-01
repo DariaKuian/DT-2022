4a5364f2ef59ae3b9a23275270bac16bfcbc71dd
dkuia <dkuia@softserveinc.com>
2022-06-01 16:51:52 +0300

    added sign up scenario

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,3 +12,10 @@ Before do
     Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
   end
 end
+
+def get_random_string(length=20)
+  source=("a".."z").to_a + ("A".."Z").to_a + (0..9).to_a
+  key=""
+  length.times{ key += source[rand(source.size)].to_s }
+  return key
+end
